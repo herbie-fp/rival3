c0eacd46f5f54feeaeb0ba71b960e66d2ad9223b
Aditya Kumar <obround.dev@gmail.com>
2026-02-03 11:46:00 -0800

    Implement Rival2 nightly for Rival3

--- src/eval/execute.rs
+++ src/eval/execute.rs
@@ -29,7 +29,8 @@ pub fn evaluate_instruction(instruction: &Instruction, registers: &mut [Ival], p
     match &instruction.data {
         Literal { value } => {
             let val = &value.0;
-            out_reg.set_prec(precision.max(val.prec()));
+            // TODO: check -- we are reverting
+            // out_reg.set_prec(precision.max(val.prec()));
             out_reg.lo.as_float_mut().assign_round(val, Round::Down);
             out_reg.hi.as_float_mut().assign_round(val, Round::Up);
             out_reg.err = ErrorFlags::none();
