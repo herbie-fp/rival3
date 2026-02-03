1f3dd7bd437cf282fd26d48ebbe108c8b1148787
Aditya Kumar <obround.dev@gmail.com>
2026-02-03 15:13:05 -0800

    Add python graphs to nightly

--- infra/nightly.sh
+++ infra/nightly.sh
@@ -16,5 +16,11 @@ rustup update
 cargo build --release --manifest-path rival3-ffi/Cargo.toml
 xz -d -k -f infra/points.json.xz
 racket -y infra/time.rkt --dir "$REPORTDIR" --profile profile.json infra/points.json
+python3 infra/ratio_plot.py -t "$REPORTDIR"/timeline.json -o "$REPORTDIR"
+python3 infra/point_graph.py -t "$REPORTDIR"/timeline.json -o "$REPORTDIR"
+python3 infra/histograms.py -t "$REPORTDIR"/timeline.json -o "$REPORTDIR"
+python3 infra/cnt_per_iters_plot.py -t "$REPORTDIR"/timeline.json -o "$REPORTDIR"
+python3 infra/repeats_plot.py -t "$REPORTDIR"/timeline.json -o "$REPORTDIR"
+python3 infra/density_plot.py -t "$REPORTDIR"/timeline.json -o "$REPORTDIR"
 cp profile.json "$REPORTDIR"/profile.json
 cp infra/profile.js "$REPORTDIR"/profile.js
