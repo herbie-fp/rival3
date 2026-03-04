5f738939c4d5058ca50cf53324dae05bd7e77277
Aditya Kumar <obround.dev@gmail.com>
2026-03-03 18:11:20 -0800

    Added rival3-racket license

--- rival3-racket/info.rkt
+++ rival3-racket/info.rkt
@@ -4,6 +4,7 @@
 (define version "1.0")
 
 (define pkg-desc "Racket bindings to Rival3")
+(define license 'MIT)
 
 (define deps '(("base" #:version "8.0") "math-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "math-doc"))
