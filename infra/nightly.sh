cba1f9ddc5703a0a4b70395c444d7bfecddd809c
Aditya Kumar <obround.dev@gmail.com>
2026-03-06 09:56:18 -0800

    Run install instead of build in nightly

--- infra/nightly.sh
+++ infra/nightly.sh
@@ -13,7 +13,7 @@ function clean {
 
 clean
 rustup update
-make build
+make install
 xz -d -k -f infra/points.json.xz
 racket -y infra/time.rkt --dir "$REPORTDIR" --profile profile.json infra/points.json
 python3 infra/ratio_plot.py -t "$REPORTDIR"/timeline.json -o "$REPORTDIR"
